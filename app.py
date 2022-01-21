635181228469e7828a7e844fa2f27ef2ce9cf6bc
cyrinemchirky <cyrine.mchirky2018@gmail.com>
2022-01-22 00:36:26 +0100

    commit

--- app.py
+++ app.py
@@ -8,14 +8,13 @@ from Resources.user import UserRegister
 from Resources.Destination import Destination, DestinationList
 from Resources.Region import Region, RegionList
 
-def create_app():
-    app=Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.secret_key = 'Cyrina is trying her best'
-    api = Api(app) 
-    return app
+app = Flask(__name__)
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+app.secret_key = 'Cyrina is trying her best'
+api = Api(app)
 
 @app.before_first_request
 def create_tables():
