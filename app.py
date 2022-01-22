49cc03e2c1cafce5684571f54c380670c0ec9c80
cyrinemchirky <cyrine.mchirky2018@gmail.com>
2022-01-22 23:09:26 +0100

    up

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from db import db
 app = Flask(__name__)
 
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 app.secret_key = 'Cyrina is trying her best'
--- db.py
+++ db.py
@@ -1,3 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 db = SQLAlchemy()
