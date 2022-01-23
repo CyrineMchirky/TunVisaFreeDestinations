79ad83112eeb7dba5fb88afb2cbc3a7a7c265a2d
cyrinemchirky <cyrine.mchirky2018@gmail.com>
2022-01-23 13:52:16 +0100

    gunicron fix

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ api.add_resource(RegionList, '/regions')
 api.add_resource(UserRegister, '/register')
 
 
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)  
-    app.run(debug=True)  
+
+from db import db
+db.init_app(app)  
+app.run(debug=True)  
